b94b506c9ba1e8a081c495e5bbfa2ce7a165ad45
Micheál Reilly-Sendal <micheal@local.dev>
2025-11-16 21:42:33 +0100

    feat: add Vercel adapter with Web Analytics and Image Optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://actuallymaybe.com',
+	output: 'static',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+		imageService: true,
+	}),
 	integrations: [mdx(), sitemap()],
 });
